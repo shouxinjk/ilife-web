216026ed3fb1e514f00aa9080dfb3ae7c9ce2002
Alex Chew <alex_chew@msn.com>
2018-09-14 12:08:45 +0800

    update info page layout

--- js/info.js
+++ js/info.js
@@ -7,14 +7,87 @@ $(document).ready(function ()
     var rootFontSize = 12 * (width / 1440);// 在1440px的屏幕基准像素为12px
     rootFontSize = rootFontSize <8 ? 8:rootFontSize;//最小为8px
     oHtml.style.fontSize = rootFontSize+ "px";
+    //设置正文部分宽度
+    var bodyWidth = 960*width/1440; //1440分辨率下显示960宽度
+    $("#body").width(bodyWidth);
+    galleryWidth = bodyWidth*0.7;//占比70%，960下宽度为672
+    galleryHeight = 9*galleryWidth/16;//宽高比为16:9
+    $("#main").width(galleryWidth);
     //处理参数
     var args = getQuery();
     var category = args["category"]; //当前目录
     var id = args["id"];//当前内容
     //加载导航和内容
     loadCategories(category);
-    loadItem(id);
+    loadItem(id);   
+    loadHosts(id);
 });
+
+var galleryWidth = 673;
+var galleryHeight = 378;
+
+//将item显示到页面
+function showContent(item){
+    //左侧：
+    //标题
+    $("#content").append("<div class='title'>"+item.title+"</div>");
+    // 标签
+    //正文及图片
+    for(var i=0;i<item.images.length;i++){
+        $("#gallery").append("<li><img src='" + item.images[i] + "' alt=''/></li>");//加载图片幻灯
+        $("#content").append("<img src='" + item.images[i] + "' width='100%'/>");//正文图片
+    }
+
+    //初始化图片幻灯
+    $('#gallery').galleryView({
+        panel_width: galleryWidth,
+        panel_height: galleryHeight,
+        frame_width: 80,
+        frame_height: 60
+    }); 
+
+    //右侧：
+    //标题
+    $("#title").html(item.title);
+    //推荐者列表
+    //标签云
+    for(var i=0;i<item.tags.length;i++){
+        $("#tags").append("<div class='tag'>" + item.tags[i] + "</div>");//加载图片幻灯
+    }
+    //随机着色
+    $("#tags").find("div").each(function(){
+        var rgb = Math.floor(Math.random()*(2<<23));
+        var bgColor = '#'+rgb.toString(16);
+        var color= '#'+(0xFFFFFF-rgb).toString(16);
+        $(this).css({"background-color":bgColor,"color":color});
+    });
+    //广告
+    //TODO
+}
+
+/*
+<view class="person" data-url="{{_key}}" catchtap='jump'>
+    <image class="person-img" mode="aspectFill" src="{{avatarUrl}}" data-url="{{_key}}" catchtap='jump'/>
+    <view class="person-name">{{nickName}}</view>
+    <!-- 关注按钮 -->
+    <view class="connect-status{{connected?'':'-pending'}}">{{connected?'已关注':'+关注'}}</view> 
+</view>  
+*/
+function showHosts(hosts){
+    var template="<div class='person'>"+
+    "<div class='logo'><img src='__imgSrc' alt='__name'/></div>"+//image
+    "<div class='name'>__name</div>"+//name
+    "<div class='connection'><button type='button' class='btn __status'>__text</button></div>"+//button
+    "</div>";
+    for(var i=0;i<hosts.length;i++){
+        var h = hosts[i];
+        var hostEl = template.replace(/__imgSrc/g,h.avatarUrl)
+            .replace(/__name/g,h.nickName)
+            .replace(/__status/g,"toconnect")
+            .replace(/__text/g,"+关注");
+        $("#author").append(hostEl);
+    }
+}
 
 function loadItem(key){//获取内容列表
     $.ajax({
@@ -27,16 +100,15 @@ function loadItem(key){//获取内容列表
     })            
 }
 
-//将item显示到页面
-function showContent(item){
-    // 头部幻灯
-    // 标题
-    $("#content").append("<div class='title'>"+item.title+"</div>");
-    // 内容详情，当前显示图片列表
-    for(var i=0;i<item.images.length;i++){
-        $("#content").append("<div><img src='" + item.images[i] + "' width='100%'/></div>");
-    }
-
+function loadHosts(itemId){//获取推荐者列表，可能有多个
+    $.ajax({
+        url:"https://data.shouxinjk.net/_db/sea/user/users/",
+        type:"get",
+        data:{itemId:itemId},
+        success:function(data){
+            showHosts(data);
+        }
+    })
 }
 
 function loadCategories(currentCategory){
