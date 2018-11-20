365ccc7b7e4c3185a3855fce5f2dd3efba305d8c
Alex Chew <alex_chew@msn.com>
2018-11-20 10:14:42 +0800

    add middle page

--- js/go.js
+++ js/go.js
@@ -1,6 +1,13 @@
+var client = "web";
 // 文档加载完毕后执行
 $(document).ready(function ()
 {
+    //根据屏幕大小判定是移动端还是桌面
+    const oHtml = document.getElementsByTagName('html')[0]
+    const width = oHtml.clientWidth;
+    if(width<800){
+       client = "wap";
+    }
     var args = getQuery();//获取参数
     var id = args["id"]?args["id"]:0;
     jump(id);
@@ -12,7 +19,14 @@ function jump(id){//获取详细内容
         type:"get",
         data:{},
         success:function(item){
-            log(item,function(){window.location.href=item.url2?item.url2:item.url});
+            logstash(item,client,"buy step2",function(){
+                if(client == "wap"){
+                    window.location.href=item.link.wap2?item.link.wap2:item.link.wap;
+                }else{
+                    window.location.href=item.link.web2?item.link.web2:item.link.web;
+                }
+                
+            });
         }
     })            
 }
